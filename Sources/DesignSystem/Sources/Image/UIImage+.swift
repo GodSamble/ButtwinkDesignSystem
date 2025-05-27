6f6abad8e82b3dd03722978e798263ecc9700a17
YoungminKo <105866831+GodSamble@users.noreply.github.com>
2025-05-27 12:28:45 +0900

    [Add] 플레이어 아이콘 추가

--- Sources/DesignSystem/Sources/Image/UIImage+.swift
+++ Sources/DesignSystem/Sources/Image/UIImage+.swift
@@ -43,6 +43,13 @@ public extension UIImage {
         public static let check_big           = UIImage(name: "ic_check_big")
         public static let mail_white          = UIImage(name: "ic_mail_white")
         public static let ic_profile          = UIImage(name: "ic_profile")
+        public static let play_white          = UIImage(name: "ic_play_white")
+        public static let skip_backward       = UIImage(name: "ic_skip_backward")
+        public static let skip_forward        = UIImage(name: "ic_skip_forward")
+        public static let pause               = UIImage(name: "ic_pause")
+        public static let cc_on               = UIImage(name: "ic_CC_on")
+        public static let cc_off              = UIImage(name: "ic_CC_off")
+        public static let option              = UIImage(name: "ic_option")
     }
  
     enum Img {
